a15b4babd1e4a34f429e05fb21b8fdf5305d1b0b
Nguyen Huy Hoang <huyhoang681997@gmail.com>
2018-08-24 20:42:10 +0700

    Hoang finish 'Reverse Indexes In Array' project

---
+++ src/com/company/Main.java
@@ -0,0 +1,43 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        int size;
+        int[] array;
+        Scanner scanner = new Scanner(System.in);
+        do {
+            System.out.print("Enter a size:");
+            size = scanner.nextInt();
+            if (size > 20)
+                System.out.println("Size does not exceed 20");
+        } while (size > 20);
+
+        array = new int[size];
+        int i = 0;
+        while (i < array.length) {
+            System.out.print("Enter element " + (i + 1) + ": ");
+            array[i] = scanner.nextInt();
+            i++;
+        }
+
+        System.out.print("\nElements in array: ");
+        for (int j = 0; j < array.length; j++) {
+            System.out.print(array[j] + " ");
+        }
+
+        for (int j = 0; j < array.length / 2; j++) {
+            int temp = array[j];
+            array[j] = array[size - 1 - j];
+            array[size - 1 - j] = temp;
+        }
+
+        System.out.print("\nReverse array: ");
+        for (int j = 0; j < array.length; j++) {
+            System.out.print(array[j] + " ");
+        }
+    }
+}
